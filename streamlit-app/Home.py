3da35e261675222795cc652d1c42d154e3bd39a5
Mohammed Main Uddin <90566661+main-github2021@users.noreply.github.com>
2024-09-07 01:35:43 +0600

    Add files via upload

---
+++ streamlit-app/Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="News and Summary App", layout="wide")
+
+st.header("Prothom Alo news Summary C201091 Mohammed Main Uddin")
+st.subheader("AI based Prothomalo news summarization")
+st.markdown("This app allows you to view news articles and generate summaries.")
+
+
